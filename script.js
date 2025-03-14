461c512fbd1a52653fc89af03b7da608145e3e68
Andrea Alin Hernandez Pacheco <94030407+AndreaHernandez123@users.noreply.github.com>
2025-03-14 13:40:23 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+// Aquí podrías agregar validación o interactividad
+document.querySelector('form').addEventListener('submit', function (event) {
+    event.preventDefault();
+    alert('Formulario enviado');
+  });
+  
